17ba82764ad36f61c1abd04d17a4d1e4d971828e
heyitsGaspar <israel.chay.32@gmail.com>
2024-11-14 12:53:13 -0600

    añadiendo css para offline

--- public/service-worker.js
+++ public/service-worker.js
@@ -7,7 +7,7 @@ const URLsToCache = [
   '/index.html', 
   '/main.382215a7.js', // Archivos esenciales
   '/static/js/bundle.js',
-  '/static/css/main.css',
+  '/static/css/main.css', // Asegúrate de que esta sea la ruta correcta al CSS
   '/offline.html', // Página de "offline" para cuando no haya conexión
   // Agrega otros recursos esenciales aquí
 ];
@@ -22,7 +22,6 @@ self.addEventListener('install', (event) => {
   );
 });
 
-// Activar el Service Worker
 // Activar el Service Worker
 self.addEventListener('activate', (event) => {
   const cacheWhitelist = [CACHE_NAME];
@@ -43,7 +42,6 @@ self.addEventListener('activate', (event) => {
   );
 });
 
-
 // Interceptar las solicitudes de red y devolver las respuestas desde el cache cuando no hay conexión
 self.addEventListener('fetch', (event) => {
   event.respondWith(
@@ -57,7 +55,14 @@ self.addEventListener('fetch', (event) => {
       return fetch(event.request).catch((error) => {
         // Si hay un error en la red (sin conexión), devolver una respuesta fallback
         console.log('Error fetching resource: ', error);
-        return caches.match('/offline.html'); // Asegúrate de tener una página de offline
+
+        // Si es un archivo esencial como el CSS, que debería estar en cache, manejamos específicamente
+        if (event.request.url.endsWith('.css')) {
+          return caches.match('/static/css/main.css'); // Ruta del CSS en caso de error
+        }
+
+        // Para otros recursos, devolver la página offline
+        return caches.match('/offline.html');
       });
     })
   );
